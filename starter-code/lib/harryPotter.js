dde4decff2c378e42c800370afb5c49b86e99a5a
Cristina Moreno <moreno.medran@gmail.com>
2017-12-19 10:49:47 +0100

    doing the harrypotterthing

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
  var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
                   "Ron Weasley", "1 March", "Remus Lupin", "10 March",
                   "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
@@ -8,3 +11,23 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+function organize(){
+    var organizedBirthdays = [];
+
+    for (var ix = 0; ix < birthdays.length; ix++){
+      if (ix %2 === 0){
+      var arrays = [birthdays[ix], birthdays[ix+1]];
+      organizedBirthdays.push(arrays);
+    }}
+
+    return organizedBirthdays;
+}
+
+organize();
+
+
+var moreBirthdays = [ "Lily Evans", "30 January", "James Potter", "27 March",
+                      "Dudley Dursley", "30 June", "Tom Riddle", "31 December" ];
+                      
+var bigArray = 
